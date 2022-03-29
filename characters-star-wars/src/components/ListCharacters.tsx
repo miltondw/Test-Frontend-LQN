85674d2c07afd9e4f683f86dc02038be3143751c
miltondw <estradamilton2001@gmail.com>
2022-03-29 01:54:18 -0500

    add typs query and context

---
+++ characters-star-wars/src/components/ListCharacters.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { List, Avatar } from "antd";
+import { DataListCharacter } from "../types/Types";
+import { useContext } from "react";
+import { CharacterContext } from "../context/CharacterContext";
+const ListCharacters = ({ personajes }: DataListCharacter) => {
+  const names = personajes.allPeople.edges.map(
+    (personaje) => personaje.node.name
+  );
+  const Personajes = personajes.allPeople.edges.filter(
+    ({ node }, index) => !names.includes(node.name, index + 1)
+  );
+
+  const { showDrawer } = useContext(CharacterContext);
+
+  return (
+    <div className="content-list">
+      <List
+        grid={{ gutter: 0, column: 3, xs: 1 }}
+        dataSource={Personajes}
+        renderItem={(personaje) => (
+          <List.Item key={personaje.node.id}>
+            <List.Item.Meta
+              avatar={<Avatar src="/Star-Wars.png" />}
+              title={
+                <button onClick={showDrawer}>
+                  <Link
+                    href={`/?character=${encodeURIComponent(
+                      personaje.node.name
+                    )}`}
+                  >
+                    {personaje.node.name}
+                  </Link>
+                </button>
+              }
+              description="You can see the details of the character"
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
+
+export default ListCharacters;
