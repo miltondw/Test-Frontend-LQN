4640a1e3f30e46118860d9ecf82a369b8145cb0a
miltondw <estradamilton2001@gmail.com>
2022-03-29 01:58:41 -0500

    add styles from card and images

---
+++ part1/A.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0 && i % 5 === 0) {
+    console.log(`${i} buzz bazz`);
+  } else if (i % 2 === 0) {
+    console.log(`${i} buzz`);
+  } else if (i % 5 === 0) {
+    console.log(`${i} bazz`);
+  }
+}
