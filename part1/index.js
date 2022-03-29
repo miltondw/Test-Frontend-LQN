0d77d2ce05d51a796af3e44c0c9bc21c003eccdf
miltondw <estradamilton2001@gmail.com>
2022-03-29 01:49:20 -0500

    consultas creadas

--- part1/index.js
+++
@@ -1,5 +0,0 @@
-for (let i = 0; i <= 100; i++) {
-  if (i % 2 === 0 && i % 5 === 0) console.log(i + " buzz bazz");
-  if (i % 2 === 0 && i % 5 !== 0) console.log(i + " buzz");
-  if (i % 2 !== 0 && i % 5 === 0) console.log(i + " bazz");
-}
